1b599cbcd77044d6f3f2f081a015549710128b19
DrakesonHu <drakeson_hu@brown.edu>
2025-03-16 17:36:18 -0400

    Trained New CNN, modified dataset, modified video reader script

--- TransferNew.m
+++ TransferNew.m
@@ -3,7 +3,7 @@
 % % CLPS 0950: Intro to Programming
 % % GitHub token: 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
 
-folderName = "PunchData2";
+folderName = "PunchDataCombined";
 imds = imageDatastore(folderName, ...
     IncludeSubfolders=true, ...
     LabelSource="foldernames");
@@ -15,7 +15,7 @@ imageAugmenter = imageDataAugmenter( ...
     'RandYTranslation',[-10 10], ...
     'RandXReflection',true);
 
-[imdsTrain,imdsValid,imdsTest] = splitEachLabel(imds,0.90,0.07,0.03,"randomized");
+[imdsTrain,imdsValid,imdsTest] = splitEachLabel(imds,0.85,0.10,0.05,"randomized");
 augimdsTrain = augmentedImageDatastore([224 224], imdsTrain, "ColorPreprocessing","gray2rgb", 'DataAugmentation', imageAugmenter);
 augimdsValid = augmentedImageDatastore([224 224], imdsValid, "ColorPreprocessing","gray2rgb");
 augimdsTest = augmentedImageDatastore([224 224], imdsTest, "ColorPreprocessing","gray2rgb");
@@ -48,52 +48,54 @@ imshow(I)
 %%
 
 augimdsTrain.MiniBatchSize = 32;
-load net_1.mat
+load net_2.mat
 options = trainingOptions("sgdm", ...
     ValidationData=augimdsValid, ...
     ValidationFrequency=5, ...
+    LearnRateSchedule="cosine", ...
     Plots="training-progress", ...
     Metrics="accuracy", ...
     Verbose=false, ...
     ExecutionEnvironment = 'gpu', ...
     MiniBatchSize=32, ...
-    MaxEpochs=30);
-net = trainnet(augimdsTrain, trainednet4,"crossentropy",options);
+    MaxEpochs=10);
+next2 = trainnet(augimdsTrain, next2,"crossentropy",options);
 
 %%
 
 load trainednet3.mat
 load trainednet3_1.mat
+%%
 
 classNames = categories(imds.Labels);
 
-YTest1 = minibatchpredict(net,augimdsTrain);
+YTest1 = minibatchpredict(next_net,augimdsTest);
 YTest1 = scores2label(YTest1,classNames);
-YTest2 = minibatchpredict(trainednet4,augimdsTrain);
+YTest2 = minibatchpredict(next2,augimdsTest);
 YTest2 = scores2label(YTest2,classNames);
 
-TTest = imdsTrain.Labels;
+TTest = imdsTest.Labels;
 figure
 subplot(1, 2, 1);
-title('trainednet4');
 confusionchart(TTest,YTest1);
+title("next_net");
 subplot(1, 2, 2);
-title('trainednet3_1');
 confusionchart(TTest,YTest2);
+title('next2');
 %%
 
-im = imread("C:\Users\drake\MATLAB\Projects\PunchNet\PunchData\PunchData\Miss\Screenshot 2025-03-11 222732.png");
-
-im = imresize(im,[224 224]);
-X = single(im);
-
-scores1 = predict(trainednet3,X);
-trainednet4scores2 = predict(trainednet3_1,X);
-[label1,score1] = scores2label(scores1,classNames);
-[label2,score2] = scores2label(scores2,classNames);
-
-figure
-imshow(im)
-title(['1: ' string(label1) + " (Score: " + gather(score1) + ")" ...
-    '2: ' string(label2) + " (Score: " + gather(score2) + ")"])
+% im = imread("C:\Users\drake\MATLAB\Projects\PunchNet\PunchData\PunchData\Miss\Screenshot 2025-03-11 222732.png");
+% 
+% im = imresize(im,[224 224]);
+% X = single(im);
+% 
+% scores1 = predict(trainednet3,X);
+% trainednet4scores2 = predict(trainednet3_1,X);
+% [label1,score1] = scores2label(scores1,classNames);
+% [label2,score2] = scores2label(scores2,classNames);
+% 
+% figure
+% imshow(im)
+% title(['1: ' string(label1) + " (Score: " + gather(score1) + ")" ...
+%     '2: ' string(label2) + " (Score: " + gather(score2) + ")"])
 
